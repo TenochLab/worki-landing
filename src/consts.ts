db7fbf390fca7cfda6036efcd67603a4ae8c3d32
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2025-06-24 23:29:30 -0600

    Implement footer and social links; refactor layout and styles

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+export const SITE_TITLE = "Work Today";
+export const SITE_DESCRIPTION = "Work Today App";
+
+export const SOCIAL = {
+  twitter: "https://twitter.com/WorkTodayApp",
+  github: "",
+  facebook: "",
+  instagram: "",
+  linkedin: "",
+  youtube: "",
+  tiktok: "",
+};
