73df6a1e5c9fd20eea445bf5ff217a77679838e6
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2025-09-02 00:30:30 -0600

    Actualización de identidad de la app

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,10 @@
-export const SITE_TITLE = "Work Today";
-export const SITE_DESCRIPTION = "Work Today App";
+export const SITE_TITLE = "Worki";
+export const SITE_DESCRIPTION = "Worki App";
+export const APP_NAME = "Worki";
+export const EMAIL = "worki@somosworki.com";
 
 export const SOCIAL = {
-  twitter: "https://twitter.com/WorkTodayApp",
+  twitter: "https://twitter.com/WorkiApp",
   github: "",
   facebook: "",
   instagram: "",
